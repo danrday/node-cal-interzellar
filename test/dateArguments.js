9796361961c72874c5fd6ed9a9deaf97a3f9a679
Dan Day <danday@gmail.com>
2016-08-31 11:56:03 -0500

    add tests for dateArguments module

---
+++ test/dateArguments.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const { assert: { isFunction } } = require('chai')
+
+const { returnDateArguments, allYears, allMonths } = require('../lib/dateArguments')
+
+describe('dateArguments', () => {
+  describe('returnDateArguments', () => {
+    it('should be a function', () => {
+      isFunction(returnDateArguments)
+    })
+    it ('should handle no arguments', () => {
+      const args = []
+      const expected = { month: 9, year: 2016 }
+      deepEqual(returnDateArguments(args), expected)
+    })
+    it ('single arg should be int between 1 and 9999', () => {
+      const args = 2016
+      const expected = allYears
+      oneOf(returnDateArguments(args).year, expected)
+    })
+    it ('throws error if single arg is not in range', () => {
+      const arg = 0
+      const expected = 'cal: year 0 not in range 1..9999'
+      deepEqual(returnDateArguments(arg), expected)
+    })
+    it ('throws error if single arg is NaN', () => {
+      const arg = 'string'
+      const expected = 'cal: year 0 not in range 1..9999'
+      deepEqual(returnDateArguments(arg), expected)
+    })
+    it ('should handle 2 arguments', () => {
+      const args = [january, 2016]
+      const expected = { month: 13, year: 2016 }
+      deepEqual(returnDateArguments(args), expected)
+    })
+    it ('when 2 arguments, first 3 letters of first arg should match first 3 letters of a month', () => {
+      const args = [januaryYYY, 2016]
+      const expected = 13
+      oneOf(returnDateArguments(args).month, expected)
+    })
+    it ('when 2 arguments, if first arg is a number, first arg should be int between 1 & 12', () => {
+      const args = [2, 2016]
+      const expected = allMonths
+      oneOf(returnDateArguments(args).month, expected)
+    })
+    it ('when 2 arguments, if first arg is a number, but not between 1 & 12 throw error', () => {
+      const args = [13, 2016]
+      const expected = 'cal: 13 is neither a month number (1..12) nor a name'
+      deepEqual(returnDateArguments(args), expected)
+    })
+    it ('second argument should be integer between 1 and 9999', () => {
+      const args = [january, 2016]
+      const expected = allYears
+      oneOf(returnDateArguments(args).year, expected)
+    })
+    it ('throws error if second arg is not in range', () => {
+      const args = [january, 0]
+      const expected = 'cal: year 0 not in range 1..9999'
+      deepEqual(returnDateArguments(arg).year, expected)
+    })
+  })
+})
--- test/zeller.js
+++ test/zeller.js
@@ -1,1 +1,44 @@
+//clean up
+
 "use strict";
+
+const { assert: { isFunction } } = require('chai')
+
+const { modYear, printDayOfWeek } = require('../lib/zeller')
+
+describe('zellers', () => {
+  describe('getDay', () => {
+    it('should be a function', () => {
+      isFunction(getDay)
+    })
+    it('should return a sunday indexed day of the week', () => {
+      strictEqual(getDay(2016, 8, 1), 1)
+      strictEqual(getDay(2016, 9, 11), 0)
+    })
+  })
+  describe('modifiedMonth', () => {
+    it('should be a function', () => {
+      isFunction(modifiedMonth)
+    })
+    it('should handle Jan', () => {
+      strictEqual(modifiedMonth(1), 13)
+    })
+    it('should handle other months', () => {
+      strictEqual(modifiedMonth(3), 3)
+    })
+  })
+  describe('modifiedYear', () => {
+    it('should be a function', () => {
+      isFunction(getDay)
+    })
+    it('should handle January', () => {
+      strictEqual(modifiedYear(2000, 1), 1999)
+    })
+    it('should handle February', () => {
+      strictEqual(modifiedYear(2000, 2), 1999)
+    })
+    it('should handle other months', () => {
+      strictEqual(modifiedYear(2000, 3), 2000)
+    })
+  })
+})
